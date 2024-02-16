8259d4ad7cd7114d6c1ab66115bb1315ee870a6a
armandoav <armando.arredondo.valle@gmail.com>
2024-02-16 15:46:55 -0600

    Refactor and reorganization

---
+++ Constants.tsx
@@ -0,0 +1,6 @@
+import { ILanguage } from "./app/interfaces/ILanguages";
+
+export const languages: ILanguage[] = [
+  { value: "en", label: "English" },
+  { value: "es", label: "Spanish" },
+];
--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,18 +1,10 @@
 import React from "react";
-import dynamic from "next/dynamic";
 import LanguageSelector from "../LanguageSelector/LanguageSelector";
 import { ChangeEvent } from "react";
-
-interface FooterProps {
-  updateLanguage: (selectedLanguage: string) => void;
-}
+import { FooterProps } from "@/app/interfaces/FooterProps";
+import { languages } from "@/Constants";
 
 export default function Footer({ updateLanguage }: FooterProps) {
-  const languages = [
-    { value: "en", label: "English" },
-    { value: "es", label: "Spanish" },
-  ];
-
   // Handler for language change
   const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
     const selectedLanguage = event.target.value;
