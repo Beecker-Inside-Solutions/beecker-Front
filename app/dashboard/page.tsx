95438d160eed85ae84af0f7e772423d7b9701249
armandoav <armando.arredondo.valle@gmail.com>
2024-02-19 15:09:18 -0600

    Implements dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,34 @@
-import React from "react";
-import AuthRoute from "../components/AuthComponent/AuthComponent";
-
+"use client";
+import React, { useState, useEffect } from "react";
+import styles from "./page.module.css";
+import useMultilingualValues from "../hooks/useMultilingualValues";
+import esValues from "@/esValues.json";
+import enValues from "@/enValues.json";
+import Footer from "../components/Footer/Footer";
+import Link from "next/link";
+import { routes } from "@/Constants";
 export default function Home() {
+  const { language, setLanguage, languageValues } = useMultilingualValues(
+    "en",
+    esValues,
+    enValues
+  );
+
+  const [userName, setUserName] = useState("");
+
   return (
     <>
-        <h1>Dashboard</h1>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles.topContainer}>
+            <h1>{languageValues.dashboard.welcome}, {userName}</h1>
+          </div>
+          <div className={styles.bottomContainer}>
+            <h1>b</h1>
+          </div>
+        </div>
+      </main>
+      <Footer updateLanguage={setLanguage} />
     </>
   );
 }
