507fd882251acf96d8ef72727a7d6d12cdc1d10f
ArmandoArV <armando.arredondo.valle@gmail.com>
2024-03-25 20:29:23 -0600

    servicedesk page

---
+++ app/dashboard/serviceDesk/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import useMultilingualValues from "../../hooks/useMultilingualValues";
+import logo from "../../../app/images/logos/logo.png";
+import LateralNavbar from "../../components/LateralNavbar/LateralNavbar";
+import RightBar from "../../components/RightBar/RightBar";
+
+import styles from "./page.module.css";
+
+export default function Home() {
+  const { language, setLanguage, languageValues } = useMultilingualValues(
+    "en",
+    require("@/esValues.json"),
+    require("@/enValues.json")
+  );
+  const [userName, setUserName] = useState("");
+  const [profileImg, setProfileImg] = useState(logo.src);
+
+  return (
+    <>
+      <LateralNavbar
+        lateralNavbar={require("@/Constants").lateralNavbarItems}
+        logo={logo.src}
+        user={{ isAdmin: false }}
+      />
+      <RightBar
+        profileName={userName}
+        profileImageUrl={profileImg}
+        logoutHeader={languageValues.rightBar.logoutHeader}
+        logoutText={languageValues.rightBar.logoutText}
+        logoutButton={languageValues.rightBar.logoutButton}
+        profileButton={languageValues.rightBar.profileButton}
+      />
+      <main className={styles.main}>
+        <table className={styles.incidentTable}>
+          <thead>
+            <tr></tr>
+          </thead>
+        </table>
+      </main>
+    </>
+  );
+}
