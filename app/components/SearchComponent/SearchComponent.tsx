c9242274f574fdac539d386e704d6c73516bf25d
ArmandoArV <armando.arredondo.valle@gmail.com>
2024-03-28 20:04:43 -0600

    adds clear button

--- app/components/SearchComponent/SearchComponent.tsx
+++ app/components/SearchComponent/SearchComponent.tsx
@@ -4,11 +4,15 @@ import styles from "./SearchComponent.module.css";
 interface SearchComponentProps {
   onSearch: (searchTerm: string) => void;
   placeholder: string;
+  searchPlaceholder?: string; // Make the prop optional
+  clearPlaceholder?: string; // Make the prop optional
 }
 
 const SearchComponent: React.FC<SearchComponentProps> = ({
   onSearch,
   placeholder,
+  searchPlaceholder,
+  clearPlaceholder,
 }) => {
   const [searchTerm, setSearchTerm] = useState("");
 
@@ -21,10 +25,13 @@ const SearchComponent: React.FC<SearchComponentProps> = ({
     onSearch(searchTerm);
   };
 
+  const handleClear = () => {
+    setSearchTerm("");
+    onSearch("");
+  };
+
   return (
-    <form onSubmit={handleSubmit}
-    className={styles.searchForm}
-    >
+    <form onSubmit={handleSubmit} className={styles.searchForm}>
       <input
         type="text"
         placeholder={placeholder}
@@ -33,7 +40,14 @@ const SearchComponent: React.FC<SearchComponentProps> = ({
         className={styles.searchInput}
       />
       <button type="submit" className={styles.searchButton}>
-        Search
+        {searchPlaceholder || "Search"}
+      </button>
+      <button
+        type="button"
+        className={styles.clearButton}
+        onClick={handleClear}
+      >
+        {clearPlaceholder || "Clear"}
       </button>
     </form>
   );
