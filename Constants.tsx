163e600467dcd817d6c9033b1e9a4c1e89147138
ArmandoArV <armando.arredondo.valle@gmail.com>
2024-06-12 20:35:20 -0600

    uses prod link

--- Constants.tsx
+++ Constants.tsx
@@ -11,8 +11,8 @@ import ukIcon from "./app/images/icons/uk.png";
 import serviceDesk from "./app/images/icons/serviceDesk.png";
 import { IDefaultColors } from "./app/interfaces/IDefaultColors";
 
-//export const apiURL = "https://d0j2wztj69.execute-api.us-east-1.amazonaws.com";
-export const apiURL = "http://localhost:3001";
+export const apiURL = "https://d0j2wztj69.execute-api.us-east-1.amazonaws.com";
+//export const apiURL = "http://localhost:3001";
 export const languages: ILanguage[] = [
   { value: "en", icon: ukIcon.src, label: "🇬🇧 English" },
   { value: "es", icon: mexicoIcon.src, label: "🇲🇽 Español" },
